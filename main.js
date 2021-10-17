ce7be03c8453d0b6f7024b499221a84dd5f866d6
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-10-17 22:02:30 +0300

    update Array tasks

--- main.js
+++ main.js
@@ -4,12 +4,18 @@
 const sum = function (a, b) {
   return a + b;
 };
+sum(5, 10); // => 15
+
 //---//
+
 // Q2.
 const average = function (a, b) {
   return (a + b) / 2;
 };
+average(20, 5); // => 12.5
+
 //---//
+
 // Q3.
 function factorialize(num) {
   if (num < 0) return -1;
@@ -17,25 +23,37 @@ function factorialize(num) {
   else {
     return num * factorialize(num - 1);
   }
-};
+}
+findFactorial(6) // => 720
+
 //---//
+
 // Q4.
 const round = function (number) {
   return Math.round(number);
 };
+round(1.7); // => 2
+
 //---//
+
 // Q5.
 const toThePowerOf = function (base, exponent) {
   return Math.pow(base, exponent);
 };
+toThePowerOf(9, 2); // => 81
+
 //---//
+
 // Q6.
 const randomNumber = function () {
   for (i = 0; i < 1; i++) {
     return Math.random(i);
   }
 };
+randomNumber(); // => 0.32678224417562673
+
 //---//
+
 // Q7.
 const oneOrZero = function (randomNumber) {
   if (randomNumber < 0.5) {
@@ -44,14 +62,20 @@ const oneOrZero = function (randomNumber) {
     return 1;
   }
 };
+oneOrZero(); // => 1
+
 //---//
+
 //Q8.
 const randomRange = function (number) {
   if (number > 0) {
     return Math.floor(Math.random() * number);
   }
 };
+randomRange(10); // => 7
+
 //---//
+
 // Q9.
 const includeOf = function (string, character) {
   for (i = 0; i < string.length; i++) {
@@ -62,7 +86,10 @@ const includeOf = function (string, character) {
     }
   }
 };
+includeOf("hello", "h"); // => true
+
 //---//
+
 /*
 note : first tray to solve it using for loop then Look 
 up the following on MDN indexOf() or includes() and 
@@ -72,17 +99,19 @@ solve it without iteration
 // Array
 
 // Q1.
-
 const addToArray = function (array, string) {
   array.push(string);
   return array;
 };
+addToArray(["Hello", "i", "am"], "John"); // => ["Hello", "i", "am", "John"]
+
 //---//
 
 // Q2.
 const convertToString = function (array) {
   console.log(array.join(" "));
 };
+convertToString(["Hello", "i", "am", "John"]); // => "Hello i am John"
 
 //---//
 
@@ -95,6 +124,8 @@ const updateValue = function (array, index, value) {
     console.log("there is no element to updated in this index");
   }
 };
+updateValue([1, 2, 3], 0, 10); // => [10, 2, 3]
+
 //---///
 
 // Q4.
@@ -107,7 +138,10 @@ const onlyString = function (array) {
   }
   return newArr;
 };
+onlyString(["one",12,"hi",true , 88 , "12"]); // => ["one","hi","12"]
+
 //---//
+
 // Q5.
 const users = [
   ["Jane", "123456"],
@@ -115,27 +149,39 @@ const users = [
 ];
 const login = function (username, password) {
   for (i = 0; i < users.length; i++) {
-    if ((users[i] == [username, password])) {
+    if ((users[i] == username, password)) {
       console.log("Login Successful");
     } else {
       console.log("Login Failed");
     }
   }
 };
+login("Jane", "123456"); // => "Login Successful"
+
 //---//
 
 // Q6.
-const maximumNumber = function (numbers) {};
+const maximumNumber = function (numbers) {
+    let maximum = numbers[0];
+  for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maximum) {
+        maximum = numbers[i]
+    } 
+  }
+  return maximum;
+};
 
 maximumNumber([0, 5, 2, 10, 8, 6]); // => 10
+
 //---//
 
 // Q7.
 const reversString = function (string) {
-    for(i=0; i<string.length; i++){
-        splitWo = string.split('');
-        reversWo = splitWo.reverse();
-        lastWo = reversWo.join('');
-        console.log(lastWo);
-    }
+  for (i = 0; i < string.length; i++) {
+    splitWord = string.split("");
+    reversletters = splitWord.reverse();
+    reversWord = reversletters.join("");
+    console.log(reversWord);
+  }
 };
+reversString("Hello"); // => "olleH"
